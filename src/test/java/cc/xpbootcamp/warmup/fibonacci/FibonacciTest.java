049e357e697e73a56936e18b22122383efc3dd8a
EasterFan <dongfang.fan@thoughtworks.com>
2020-02-14 20:33:50 +0800

    init test

---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -0,0 +1,32 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+    @Test
+    public void should_return_error_when_given_error_input() {
+
+    }
+
+    @Test
+    public void should_return_error_when_given_0() {
+
+    }
+
+    @Test
+    public void should_return_1_when_given_1() {
+
+    }
+
+    @Test
+    public void should_return_12586269025L_when_given_50() {
+
+    }
+
+    @Test
+    public void should_return_error_when_given_51() {
+
+    }
+
+}
