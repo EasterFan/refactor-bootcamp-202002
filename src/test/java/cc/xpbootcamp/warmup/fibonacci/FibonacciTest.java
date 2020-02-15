d5851ce83411c68148fa3704cee31d7bb6298e4a
EasterFan <dongfang.fan@thoughtworks.com>
2020-02-15 10:04:28 +0800

    finish 5 test

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -1,32 +1,41 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class FibonacciTest {
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
 
     @Test
-    public void should_return_error_when_given_error_input() {
-
+    void should_return_1_when_given_1() throws Exception {
+        int index = 1;
+        assertEquals(1, Fibonacci.fibonacci(index));
     }
 
     @Test
-    public void should_return_error_when_given_0() {
-
+    void should_return_1_when_given_2() throws Exception {
+        int index = 2;
+        assertEquals(1, Fibonacci.fibonacci(index));
     }
 
     @Test
-    public void should_return_1_when_given_1() {
-
+    void should_return_144_when_given_12() throws Exception {
+        int index = 12;
+        assertEquals(144, Fibonacci.fibonacci(index));
     }
 
     @Test
-    public void should_return_12586269025L_when_given_50() {
-
+    void should_return_12586269025L_when_given_50() throws Exception {
+        int index = 50;
+        assertEquals(12586269025L, Fibonacci.fibonacci(index));
     }
 
     @Test
-    public void should_return_error_when_given_51() {
-
+    void should_return_error_when_given_error_input() {
+        int index = -1;
+        Exception exception = assertThrows(Exception.class, () ->
+                Fibonacci.fibonacci(index));
+        assertEquals("Bad args", exception.getMessage());
     }
-
 }
