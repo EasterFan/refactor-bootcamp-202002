6f871839ca4247c9091428329753b5e7c8db12c8
EasterFan <dongfang.fan@thoughtworks.com>
2020-02-19 14:51:30 +0800

    seperate constant

---
+++ src/main/java/cc/xpbootcamp/warmup/cashier/Constant.java
@@ -0,0 +1,7 @@
+package cc.xpbootcamp.warmup.cashier;
+
+public class Constant {
+    public static final String XINGQI = "星期";
+    public static final String[] WEEKS = new String[]{"日", "一", "二", "三", "四", "五", "六"};
+    public static final String YY_MM_DD = "yyyy年MM月dd日";
+}
