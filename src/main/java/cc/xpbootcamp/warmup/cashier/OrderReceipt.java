ddac32e83a92156a8bf9093e2632569ae5ed5d64
EasterFan <dongfang.fan@thoughtworks.com>
2020-02-19 14:51:57 +0800

    refactor orderreceipt

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -2,41 +2,76 @@ package cc.xpbootcamp.warmup.cashier;
 
 import lombok.AllArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static cc.xpbootcamp.warmup.cashier.Constant.*;
+
 @AllArgsConstructor
 public class OrderReceipt {
-    private Order order;
     private static final double SALES_TAX_RATE = 0.1;
+    private static final double DISCOUNT_RATE = 0.02;
+    private Order order;
 
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        output.append("======Printing Orders======\n");
+        printTitle(output);
+        printDate(output);
+        printCustomInfo(output);
+        printLineItems(output);
+        calculateAndPrintTaxAndAmount(output);
+        return output.toString();
+    }
 
+    private void printTitle(StringBuilder output) {
+        output.append("===== 老王超市，值得信赖 ======\n");
+    }
+
+    private void printDate(StringBuilder output) {
+        output.append(DateTimeFormatter.ofPattern(YY_MM_DD).format(LocalDate.now()))
+                .append(", ")
+                .append(XINGQI).append(WEEKS[LocalDate.now().getDayOfWeek().getValue()])
+                .append('\n');
+    }
+
+    private void printCustomInfo(StringBuilder output) {
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
+    }
 
-        double totSalesTx = 0d;
-        double totalPrice = 0d;
+    private void printLineItems(StringBuilder output) {
         for (LineItem lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
-
-            double salesTax = lineItem.totalAmount() * SALES_TAX_RATE;
-            totSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            totalPrice += lineItem.totalAmount() + salesTax;
+            output.append('\n' + lineItem.getDescription()).append(",").append('\t')
+                    .append(lineItem.getPrice()).append('x').append(lineItem.getQuantity())
+                    .append('\t');
+            output.append(lineItem.totalAmount()).append('\t');
         }
+    }
 
-        output.append("Sales Tax").append('\t').append(totSalesTx);
+    private void printTaxAndTotalPrice(StringBuilder output, double totalSalesTax, double totalAmount) {
+        output.append("\n税额: ").append(totalSalesTax).append('\n');
+        double discount = 0;
+        if (LocalDate.now().getDayOfWeek().getValue() == 3) {
+            discount = totalAmount * DISCOUNT_RATE;
+            output.append("折扣: ")
+                    .append(BigDecimal.valueOf(discount).setScale(2, RoundingMode.HALF_UP).doubleValue())
+                    .append('\n');
+        }
+        output.append("总价: ")
+                .append(BigDecimal.valueOf(totalAmount + totalSalesTax - discount).setScale(2, RoundingMode.HALF_UP).doubleValue())
+                .append('\n');
+    }
 
-        output.append("Total Amount").append('\t').append(totalPrice);
-        return output.toString();
+    private void calculateAndPrintTaxAndAmount(StringBuilder output) {
+        double totalSalesTax = 0d;
+        double totalAmount = 0d;
+        for (LineItem lineItem : order.getLineItems()) {
+            totalSalesTax += lineItem.totalAmount() * SALES_TAX_RATE;
+            totalAmount += lineItem.totalAmount();
+        }
+        printTaxAndTotalPrice(output, totalSalesTax, totalAmount);
     }
 }
